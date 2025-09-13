22eed49fb3b5e60bc266c8828b2a88fc55c3fec0
Manan Mishra <146760182+MananHere576@users.noreply.github.com>
2025-09-13 12:17:09 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:npg_aXxHPLy3w4eC@ep-ancient-lab-ad12aj9i-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
   };
